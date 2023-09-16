ce60a0c158f45f582d74b7a12960d822c8e3e10c
Lauti204 <taboaa2010@gmail.com>
2023-09-16 20:40:30 -0300

    Primera subida...

--- TPWinForm_equipo-9/Form1.Designer.cs
+++ TPWinForm_equipo-9/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace TPWinForm_equipo_9
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnPrueba = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -90,11 +91,21 @@ namespace TPWinForm_equipo_9
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
             // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(347, 12);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(134, 42);
+            this.btnPrueba.TabIndex = 6;
+            this.btnPrueba.Text = "Prueba";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPrueba);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
@@ -117,6 +128,7 @@ namespace TPWinForm_equipo_9
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnPrueba;
     }
 }
 
