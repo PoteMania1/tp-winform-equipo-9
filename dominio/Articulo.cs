0a61390dbbd105ad053d05834aa033767fb97c2c
PoteMania1 <mauricio.villalba@alumnos.frgp.utn.edu.ar>
2023-09-16 20:53:22 -0300

    clase articulo

---
+++ dominio/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    class Articulo
+    {
+        public int CodigodeArticulo;
+        public string Nombre;
+        public string Descripcion;
+        public int Precio;
+        //Marca
+        //Categoría
+        //public Imagen;
+
+    }
+}
